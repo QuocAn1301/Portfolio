566b623cab31c7e8176ac89c52beb5afcbc74333
QuocAn1301 <137316838+QuocAn1301@users.noreply.github.com>
2024-07-22 03:14:12 +0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,4 +67,71 @@ function portfolioItemDetails(portfolioItem) {
     document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
 }
 
+// infut animation
+const inputs = document.querySelectorAll(".input")
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+});
+
+/* SCROLL SECTIONS ACTIVE LINK */
+// get all sections that have an id defined 
+const sections = document.querySelectorAll("section[id]");
+
+// add an event listener listening for scroll 
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+    // get current scroll position
+    let scrollY = window.pageYOffset;
+
+    // Now we loop through sections to get height, top and ID values for each
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute("id");
+
+        // Determine if current scroll position is within this section
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            // Add active class to corresponding navigation Link
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link");
+        } else {
+            // Remove active class from corresponding navigation Link
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link");
+        }
+    });
+}
+
+// -SHOW SIDEBAR 
+const navMenu = document.getElementById('sidebar'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+/*===== SIDEBAR SHOW
+/* Validate If Constant Exists */
+if (navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add('show-sidebar')
+    })
+}
+// ===== SIDEBAR HIDDEN =====*/
+/* Validate If Constant Exists */
+if (navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove('show-sidebar')
+    })
+}
+
+
 
